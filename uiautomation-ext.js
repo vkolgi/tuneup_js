70d8ee2043f0cad995213c58068c6c37e39d05ce
PokerChang <chang@pokerchang.com>
2011-09-08 16:42:42 -0700

    Added typeString to UIATextField and UIATextView in order to simulate keyboard typing

--- uiautomation-ext.js
+++ uiautomation-ext.js
@@ -46,7 +46,7 @@ extend(UIAElement.prototype, {
    * A shortcut for waiting an element to become visible and tap.
    */
   vtap: function() {
-    this.waitForVisible(5, 0.25);
+    this.waitForVisible(10, 0.25);
     this.tap();
   },
   /**
@@ -144,4 +144,105 @@ extend(UIATarget.prototype, {
       return this.model().match(/^iPhone/) !== null;
     }
 });
+extend(UIAKeyboard.prototype,{
+	KEYBOARD_TYPE_UNKNOWN :-1,
+	KEYBOARD_TYPE_ALPHA : 0,
+	KEYBOARD_TYPE_ALPHA_CAPS : 1,
+	KEYBOARD_TYPE_NUMBER_AND_PUNCTUATION:2,
+	KEYBOARD_TYPE_NUMBER:3,
+	keyboardType : function()
+	    {
+			if (this.keys().length < 12){
+	            return this.KEYBOARD_TYPE_NUMBER;
+			}
+	        else if (this.keys().firstWithName("a").toString() != "[object UIAElementNil]")
+	            return this.KEYBOARD_TYPE_ALPHA;
+	        else if (this.keys().firstWithName("A").toString() != "[object UIAElementNil]")
+	            return this.KEYBOARD_TYPE_ALPHA_CAPS;
+	        else if (this.keys().firstWithName("1").toString() != "[object UIAElementNil]")
+	            return this.KEYBOARD_TYPE_NUMBER_AND_PUNCTUATION;
+	        else
+	            return this.KEYBOARD_TYPE_UNKNOWN;
+	    }
+	
+});
 
+/*
+TODO: Character keyboard is super slow.
+*/
+var typeString = function(pstrString, pbClear)
+{
+    if (!this.hasKeyboardFocus())
+        this.tap();
+
+    UIATarget.localTarget().delay(0.5);
+
+    if (pbClear || pstrString.length == 0)
+        this.clear();
+
+    if (pstrString.length > 0)
+    {
+        var app = UIATarget.localTarget().frontMostApp();
+        var keyboard = app.keyboard();
+        var keys = app.keyboard().keys();
+        var buttons = app.keyboard().buttons();
+        for (i = 0; i < pstrString.length; i++)
+        {
+	        var intKeyboardType = keyboard.keyboardType();
+	        var bIsAllCaps = (intKeyboardType == keyboard.KEYBOARD_TYPE_ALPHA_CAPS); //Handles autocapitalizationType = UITextAutocapitalizationTypeAllCharacters
+	        var intNewKeyboardType = intKeyboardType;
+            var strChar = pstrString.charAt(i);
+            if ((/[a-z]/.test(strChar)) && intKeyboardType == keyboard.KEYBOARD_TYPE_ALPHA_CAPS && !bIsAllCaps)
+            {
+                buttons.firstWithName("shift").tap();
+                intKeyboardType = keyboard.KEYBOARD_TYPE_ALPHA;
+            }
+            else if ((/[A-Z]/.test(strChar)) && intKeyboardType == keyboard.KEYBOARD_TYPE_ALPHA)
+            {
+                buttons.firstWithName("shift").tap();
+                intKeyboardType = keyboard.KEYBOARD_TYPE_ALPHA_CAPS;
+            }
+            else if ((/[A-z]/.test(strChar)) && intKeyboardType == keyboard.KEYBOARD_TYPE_NUMBER_AND_PUNCTUATION)
+            {
+                buttons.firstWithName("more, letters").tap();
+                intKeyboardType = keyboard.KEYBOARD_TYPE_ALPHA;
+            }
+            else if ((/[0-9.]/.test(strChar)) && intKeyboardType != keyboard.KEYBOARD_TYPE_NUMBER_AND_PUNCTUATION && intKeyboardType != keyboard.KEYBOARD_TYPE_NUMBER)
+            {
+                buttons.firstWithName("more, numbers").tap();
+                intKeyboardType = keyboard.KEYBOARD_TYPE_NUMBER_AND_PUNCTUATION;
+            }
+
+            if ((/[a-z]/.test(strChar)) && intKeyboardType == keyboard.KEYBOARD_TYPE_ALPHA_CAPS)
+                strChar = strChar.toUpperCase();
+            if (strChar == " ")
+                keys["space"].tap();
+            else if (/[0-9]/.test(strChar)) // Need to change strChar to the index key of the number because strChar = "0" will tap "1" and strChar = "1" will tap "2"
+            {
+               	if (strChar == "0")
+					if (intKeyboardType == keyboard.KEYBOARD_TYPE_NUMBER_AND_PUNCTUATION){
+                    	strChar = "9";
+					}
+					else{
+						strChar = "10";
+					}
+               	else
+                   	strChar = (parseInt(strChar) - 1).toString();	
+
+                keys[strChar].tap();
+            }
+            else{
+                keys[strChar].tap(); // TODO: this line is super slow when there are many keys
+			}
+            UIATarget.localTarget().delay(0.5);
+
+        }
+    }
+};
+
+extend(UIATextField.prototype,{
+	typeString: typeString,
+});
+extend(UIATextView.prototype,{
+	typeString: typeString
+});
