a3109011c078a56c340abef06d6eff63b33348c6
Max Rabin <max@glidetalk.com>
2012-10-17 11:37:27 +0200

    Clean up the code to look closer like common Javascript styles (jslint/jshint)

--- uiautomation-ext.js
+++ uiautomation-ext.js
@@ -20,35 +20,33 @@ extend(UIATableView.prototype, {
 });
 
 extend(UIAElement.prototype, {
-	// Poll till the item becomes visible, up to a specified timeout
-	waitUntilVisible: function (timeoutInSeconds)
-	{
-		timeoutInSeconds = timeoutInSeconds == null ? 5 : timeoutInSeconds;
-		var element = this;
-		var delay = 0.25;
-		retry(function() { 
-			if(!element.isVisible()) {
-				throw("Element (" +  element + ") didn't become visible within " + timeoutInSeconds + " seconds.");
-			}
-		}, timeoutInSeconds/delay, delay);
-	},
+  // Poll till the item becomes visible, up to a specified timeout
+  waitUntilVisible: function (timeoutInSeconds) {
+    timeoutInSeconds = timeoutInSeconds == null ? 5 : timeoutInSeconds;
+    var element = this;
+    var delay = 0.25;
+    retry(function() { 
+      if(!element.isVisible()) {
+        throw("Element (" +  element + ") didn't become visible within " + timeoutInSeconds + " seconds.");
+      }
+    }, timeoutInSeconds/delay, delay);
+  },
 
-	/*
-		Wait until element becomes invisible
-	*/	
-	waitUntilInvisible: function (timeoutInSeconds)
-	{
-		timeoutInSeconds = timeoutInSeconds == null ? 5 : timeoutInSeconds;
-		var element = this;
-		var delay = 0.25;
-		retry(function() { 
-			if(element.isVisible()) {
-				throw("Element (" +  element + ") didn't become invisible within " + timeoutInSeconds + " seconds.");
-			}
-		}, timeoutInSeconds/delay, delay);
-	},
-	
-	/**
+  /*
+  Wait until element becomes invisible
+  */  
+  waitUntilInvisible: function (timeoutInSeconds) {
+    timeoutInSeconds = timeoutInSeconds == null ? 5 : timeoutInSeconds;
+    var element = this;
+    var delay = 0.25;
+    retry(function() { 
+      if(element.isVisible()) {
+        throw("Element (" +  element + ") didn't become invisible within " + timeoutInSeconds + " seconds.");
+      }
+    }, timeoutInSeconds/delay, delay);
+  },
+  
+  /**
    * A shortcut for waiting an element to become visible and tap.
    */
   vtap: function() {
@@ -81,21 +79,21 @@ extend(UIAApplication.prototype, {
    * A shortcut for checking that the interface orientation in either
    * portrait mode
    */
-   isPortraitOrientation: function() {
-     var orientation = this.interfaceOrientation();
-     return orientation == UIA_DEVICE_ORIENTATION_PORTRAIT ||
-       orientation == UIA_DEVICE_ORIENTATION_PORTRAIT_UPSIDEDOWN;
-   },
+  isPortraitOrientation: function() {
+    var orientation = this.interfaceOrientation();
+    return orientation == UIA_DEVICE_ORIENTATION_PORTRAIT ||
+      orientation == UIA_DEVICE_ORIENTATION_PORTRAIT_UPSIDEDOWN;
+  },
 
   /**
    * A shortcut for checking that the interface orientation in one of the
    * landscape orientations.
    */
-   isLandscapeOrientation: function() {
-     var orientation = this.interfaceOrientation();
-     return orientation == UIA_DEVICE_ORIENTATION_LANDSCAPELEFT ||
-       orientation == UIA_DEVICE_ORIENTATION_LANDSCAPERIGHT;
-   }
+  isLandscapeOrientation: function() {
+    var orientation = this.interfaceOrientation();
+    return orientation == UIA_DEVICE_ORIENTATION_LANDSCAPELEFT ||
+      orientation == UIA_DEVICE_ORIENTATION_LANDSCAPERIGHT;
+  }
 });
 
 extend(UIANavigationBar.prototype, {
@@ -119,138 +117,124 @@ extend(UIATarget.prototype, {
    * A shortcut for checking that the interface orientation in either
    * portrait mode
    */
-   isPortraitOrientation: function() {
-     var orientation = this.deviceOrientation();
-     return orientation == UIA_DEVICE_ORIENTATION_PORTRAIT ||
-       orientation == UIA_DEVICE_ORIENTATION_PORTRAIT_UPSIDEDOWN;
+  isPortraitOrientation: function() {
+    var orientation = this.deviceOrientation();
+    return orientation == UIA_DEVICE_ORIENTATION_PORTRAIT ||
+      orientation == UIA_DEVICE_ORIENTATION_PORTRAIT_UPSIDEDOWN;
    },
 
   /**
    * A shortcut for checking that the interface orientation in one of the
    * landscape orientations.
    */
-   isLandscapeOrientation: function() {
-     var orientation = this.deviceOrientation();
-     return orientation == UIA_DEVICE_ORIENTATION_LANDSCAPELEFT ||
-       orientation == UIA_DEVICE_ORIENTATION_LANDSCAPERIGHT;
+  isLandscapeOrientation: function() {
+    var orientation = this.deviceOrientation();
+    return orientation == UIA_DEVICE_ORIENTATION_LANDSCAPELEFT ||
+      orientation == UIA_DEVICE_ORIENTATION_LANDSCAPERIGHT;
    },
 
    /**
-    * A convenience method for detecting that you're running on an iPad
-    */
-    isDeviceiPad: function() {
-      return this.model().match(/^iPad/) !== null;
-    },
+  * A convenience method for detecting that you're running on an iPad
+  */
+  isDeviceiPad: function() {
+    return this.model().match(/^iPad/) !== null;
+  },
 
-    /**
-     * A convenience method for detecting that you're running on an
-     * iPhone or iPod touch
-     */
-    isDeviceiPhone: function() {
-      return this.model().match(/^iPhone/) !== null;
-    }
+  /**
+   * A convenience method for detecting that you're running on an
+   * iPhone or iPod touch
+   */
+  isDeviceiPhone: function() {
+    return this.model().match(/^iPhone/) !== null;
+  }
 });
 extend(UIAKeyboard.prototype,{
-	KEYBOARD_TYPE_UNKNOWN :-1,
-	KEYBOARD_TYPE_ALPHA : 0,
-	KEYBOARD_TYPE_ALPHA_CAPS : 1,
-	KEYBOARD_TYPE_NUMBER_AND_PUNCTUATION:2,
-	KEYBOARD_TYPE_NUMBER:3,
-	keyboardType : function()
-	    {
-			if (this.keys().length < 12){
-	            return this.KEYBOARD_TYPE_NUMBER;
-			}
-	        else if (this.keys().firstWithName("a").toString() != "[object UIAElementNil]")
-	            return this.KEYBOARD_TYPE_ALPHA;
-	        else if (this.keys().firstWithName("A").toString() != "[object UIAElementNil]")
-	            return this.KEYBOARD_TYPE_ALPHA_CAPS;
-	        else if (this.keys().firstWithName("1").toString() != "[object UIAElementNil]")
-	            return this.KEYBOARD_TYPE_NUMBER_AND_PUNCTUATION;
-	        else
-	            return this.KEYBOARD_TYPE_UNKNOWN;
-	    }
-	
+  KEYBOARD_TYPE_UNKNOWN :-1,
+  KEYBOARD_TYPE_ALPHA : 0,
+  KEYBOARD_TYPE_ALPHA_CAPS : 1,
+  KEYBOARD_TYPE_NUMBER_AND_PUNCTUATION:2,
+  KEYBOARD_TYPE_NUMBER:3,
+  keyboardType : function() {
+  if (this.keys().length < 12){
+    return this.KEYBOARD_TYPE_NUMBER;
+  } else if (this.keys().firstWithName("a").toString() != "[object UIAElementNil]") {
+    return this.KEYBOARD_TYPE_ALPHA;
+  } else if (this.keys().firstWithName("A").toString() != "[object UIAElementNil]") {
+    return this.KEYBOARD_TYPE_ALPHA_CAPS;
+  } else if (this.keys().firstWithName("1").toString() != "[object UIAElementNil]") {
+    return this.KEYBOARD_TYPE_NUMBER_AND_PUNCTUATION;
+  } else {
+    return this.KEYBOARD_TYPE_UNKNOWN;
+  }
+  }
 });
 
 /*
 TODO: Character keyboard is super slow.
 */
-var typeString = function(pstrString, pbClear)
-{
-	pstrString += ''; // convert number to string 
-	
-	if (!this.hasKeyboardFocus())
-        this.tap();
+var typeString = function(pstrString, pbClear) {
+  pstrString += ''; // convert number to string 
+  if (!this.hasKeyboardFocus()){
+    this.tap();
+  }
 
-    UIATarget.localTarget().delay(0.5);
+  UIATarget.localTarget().delay(0.5);
 
-    if (pbClear || pstrString.length == 0)
-        this.clear();
+  if (pbClear || pstrString.length == 0) {
+    this.clear();
+  }
 
-    if (pstrString.length > 0)
-    {
-        var app = UIATarget.localTarget().frontMostApp();
-        var keyboard = app.keyboard();
-        var keys = app.keyboard().keys();
-        var buttons = app.keyboard().buttons();
-        for (i = 0; i < pstrString.length; i++)
-        {
-	        var intKeyboardType = keyboard.keyboardType();
-	        var bIsAllCaps = (intKeyboardType == keyboard.KEYBOARD_TYPE_ALPHA_CAPS); //Handles autocapitalizationType = UITextAutocapitalizationTypeAllCharacters
-	        var intNewKeyboardType = intKeyboardType;
-            var strChar = pstrString.charAt(i);
-            if ((/[a-z]/.test(strChar)) && intKeyboardType == keyboard.KEYBOARD_TYPE_ALPHA_CAPS && !bIsAllCaps)
-            {
-                buttons.firstWithName("shift").tap();
-                intKeyboardType = keyboard.KEYBOARD_TYPE_ALPHA;
-            }
-            else if ((/[A-Z]/.test(strChar)) && intKeyboardType == keyboard.KEYBOARD_TYPE_ALPHA)
-            {
-                buttons.firstWithName("shift").tap();
-                intKeyboardType = keyboard.KEYBOARD_TYPE_ALPHA_CAPS;
-            }
-            else if ((/[A-z]/.test(strChar)) && intKeyboardType == keyboard.KEYBOARD_TYPE_NUMBER_AND_PUNCTUATION)
-            {
-                buttons.firstWithName("more, letters").tap();
-                intKeyboardType = keyboard.KEYBOARD_TYPE_ALPHA;
-            }
-            else if ((/[0-9.]/.test(strChar)) && intKeyboardType != keyboard.KEYBOARD_TYPE_NUMBER_AND_PUNCTUATION && intKeyboardType != keyboard.KEYBOARD_TYPE_NUMBER)
-            {
-                buttons.firstWithName("more, numbers").tap();
-                intKeyboardType = keyboard.KEYBOARD_TYPE_NUMBER_AND_PUNCTUATION;
-            }
+  if (pstrString.length > 0) {
+    var app = UIATarget.localTarget().frontMostApp();
+    var keyboard = app.keyboard();
+    var keys = app.keyboard().keys();
+    var buttons = app.keyboard().buttons();
+    for (i = 0; i < pstrString.length; i++) {
+      var intKeyboardType = keyboard.keyboardType();
+      var bIsAllCaps = (intKeyboardType == keyboard.KEYBOARD_TYPE_ALPHA_CAPS); //Handles autocapitalizationType = UITextAutocapitalizationTypeAllCharacters
+      var intNewKeyboardType = intKeyboardType;
+      var strChar = pstrString.charAt(i);
+      if ((/[a-z]/.test(strChar)) && intKeyboardType == keyboard.KEYBOARD_TYPE_ALPHA_CAPS && !bIsAllCaps) {
+        buttons.firstWithName("shift").tap();
+        intKeyboardType = keyboard.KEYBOARD_TYPE_ALPHA;
+      } else if ((/[A-Z]/.test(strChar)) && intKeyboardType == keyboard.KEYBOARD_TYPE_ALPHA) {
+        buttons.firstWithName("shift").tap();
+        intKeyboardType = keyboard.KEYBOARD_TYPE_ALPHA_CAPS;
+      } else if ((/[A-z]/.test(strChar)) && intKeyboardType == keyboard.KEYBOARD_TYPE_NUMBER_AND_PUNCTUATION) {
+        buttons.firstWithName("more, letters").tap();
+        intKeyboardType = keyboard.KEYBOARD_TYPE_ALPHA;
+      } else if ((/[0-9.]/.test(strChar)) && intKeyboardType != keyboard.KEYBOARD_TYPE_NUMBER_AND_PUNCTUATION && intKeyboardType != keyboard.KEYBOARD_TYPE_NUMBER) {
+        buttons.firstWithName("more, numbers").tap();
+        intKeyboardType = keyboard.KEYBOARD_TYPE_NUMBER_AND_PUNCTUATION;
+      }
 
-            if ((/[a-z]/.test(strChar)) && intKeyboardType == keyboard.KEYBOARD_TYPE_ALPHA_CAPS)
-                strChar = strChar.toUpperCase();
-            if (strChar == " ")
-                keys["space"].tap();
-            else if (/[0-9]/.test(strChar)) // Need to change strChar to the index key of the number because strChar = "0" will tap "1" and strChar = "1" will tap "2"
-            {
-               	if (strChar == "0")
-					if (intKeyboardType == keyboard.KEYBOARD_TYPE_NUMBER_AND_PUNCTUATION){
-                    	strChar = "9";
-					}
-					else{
-						strChar = "10";
-					}
-               	else
-                   	strChar = (parseInt(strChar) - 1).toString();	
-
-                keys[strChar].tap();
-            }
-            else{
-                keys[strChar].tap(); // TODO: this line is super slow when there are many keys
-			}
-            UIATarget.localTarget().delay(0.5);
-
+      if ((/[a-z]/.test(strChar)) && intKeyboardType == keyboard.KEYBOARD_TYPE_ALPHA_CAPS) {
+        strChar = strChar.toUpperCase();
+      }
+      if (strChar == " ") {
+        keys["space"].tap();
+      } else if (/[0-9]/.test(strChar)) { // Need to change strChar to the index key of the number because strChar = "0" will tap "1" and strChar = "1" will tap "2"
+        if (strChar == "0") {
+          if (intKeyboardType == keyboard.KEYBOARD_TYPE_NUMBER_AND_PUNCTUATION) {
+            strChar = "9";
+          } else {
+            strChar = "10";
+          } else { 
+            strChar = (parseInt(strChar) - 1).toString();
+          }
         }
+        keys[strChar].tap();
+      } else {
+        keys[strChar].tap(); // TODO: this line is super slow when there are many keys
+      }
+      UIATarget.localTarget().delay(0.5);
     }
+  }
 };
 
 extend(UIATextField.prototype,{
-	typeString: typeString,
+  typeString: typeString,
 });
 extend(UIATextView.prototype,{
-	typeString: typeString
+  typeString: typeString
 });
