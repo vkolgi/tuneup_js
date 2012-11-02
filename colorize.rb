e78051cac83f729b37999ef6077764fb03d02e92
Sebastian Ludwig <sebastian@lurado.de>
2012-11-03 00:47:39 +0100

    Colorized output

---
+++ colorize.rb
@@ -0,0 +1,24 @@
+# based on http://stackoverflow.com/questions/1489183/colorized-ruby-output
+
+class String
+  # colorization
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def red
+    colorize(31)
+  end
+
+  def green
+    colorize(32)
+  end
+
+  def yellow
+    colorize(33)
+  end
+
+  def cyan
+    colorize(36)
+  end
+end
