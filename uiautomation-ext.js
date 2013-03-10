27d13c2097ed9cf30a77b0f4ab2e0fbb95e59661
Alex Vollmer <alex@radiantcapsule.com>
2013-03-10 14:32:30 -0700

    Added a function to scroll picker wheels (also texts) -> does not work for DatePickers Added a function to get "real" values of picker wheels
    Conflicts:
    	uiautomation-ext.js
    
    Manually merged from https://github.com/alexvollmer/tuneup_js/pull/20

--- uiautomation-ext.js
+++ uiautomation-ext.js
@@ -41,7 +41,7 @@ extend(UIAElement.prototype, {
             return !element.isVisible();
         }, timeoutInSeconds, "to become invisible");
     },
-    
+
     /**
      * Wait until child element with name is added
      */
@@ -52,7 +52,7 @@ extend(UIAElement.prototype, {
             return element.isValid();
         }, timeoutInSeconds, "to become valid");
     },
-    
+
     /**
      * Wait until child element with name is removed
      */
@@ -63,7 +63,7 @@ extend(UIAElement.prototype, {
             return !element.isValid();
         }, timeoutInSeconds, "to become invalid");
     },
-    
+
     /**
      * Wait until element fulfills condition
      */
@@ -78,7 +78,7 @@ extend(UIAElement.prototype, {
             }
         }, Math.max(1, timeoutInSeconds/delay), delay);
     },
-	
+
   /**
    * A shortcut for waiting an element to become visible and tap.
    */
@@ -136,7 +136,7 @@ extend(UIANavigationBar.prototype, {
   assertLeftButtonNamed: function(name) {
     assertEquals(name, this.leftButton().name());
   },
-  
+
   /**
    * Asserts that the right button's name matches the given +name+ argument
    */
@@ -272,5 +272,76 @@ extend(UIATextField.prototype,{
   typeString: typeString
 });
 extend(UIATextView.prototype,{
-  typeString: typeString
+	typeString: typeString
+});
+
+extend(UIAPickerWheel.prototype, {
+
+   /*
+    * Better implementation than UIAPickerWheel.selectValue
+    * Works also for texts
+    * Poorly works not for UIDatePickers -> because .values() which get all values of wheel does not work :(
+    * I think this is a bug in UIAutomation!
+    */
+   scrollToValue: function (valueToSelect) {
+
+      var element = this;
+
+      var values = this.values();
+      var pickerValue = element.value();
+
+      // convert to string
+      valueToSelect = valueToSelect + "";
+
+      // some wheels return for .value()  "17. 128 of 267" ?? don't know why
+      // throw away all after "." but be careful lastIndexOf is used because the value can
+      // also have "." in it!! e.g.: "1.2. 13 of 27"
+      if (pickerValue.lastIndexOf(".") != -1) {
+        var currentValue = pickerValue.substr(0, pickerValue.lastIndexOf("."));
+      } else {
+        var currentValue = element.value();
+      }
+
+      var currentValueIndex = values.indexOf(currentValue);
+      var valueToSelectIndex = values.indexOf(valueToSelect);
+
+      if (valueToSelectIndex == -1) {
+        fail("value: " + valueToSelect + " not found in Wheel!");
+      }
+
+      var elementsToScroll = valueToSelectIndex - currentValueIndex;
+
+      UIALogger.logDebug("number of elements to scroll: " + elementsToScroll);
+      if (elementsToScroll > 0) {
+
+          for (i=0; i<elementsToScroll; i++) {
+            element.tapWithOptions({tapOffset:{x:0.35, y:0.67}});
+            target.delay(0.7);
+          }
+
+      } else {
+
+          for (i=0; i>elementsToScroll; i--) {
+            element.tapWithOptions({tapOffset:{x:0.35, y:0.31}});
+            target.delay(0.7);
+          }
+      }
+   },
+
+   /*
+    * Wheels filled with values return for .value()  "17. 128 of 267" ?? don't know why -> for comparisons this is unuseful!!
+    * If you want to check a value of a wheel this function is very helpful
+    */
+   realValue: function() {
+
+      // current value of wheel
+      var pickerValue = this.value();
+
+      // throw away all after "." but be careful lastIndexOf is used because the value can
+      if (pickerValue.lastIndexOf(".") != -1) {
+        return pickerValue.substr(0, pickerValue.lastIndexOf("."));
+      }
+
+      return this.value();
+   }
 });
