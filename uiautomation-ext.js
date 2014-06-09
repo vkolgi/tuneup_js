df02f61ebfd5a7042aee74d64f62b5f3681459b3
Ian Katz <iakatz@paypal.com>
2014-06-09 17:37:48 -0400

    Add check for non-UIAElement objects

--- uiautomation-ext.js
+++ uiautomation-ext.js
@@ -32,7 +32,7 @@ extend(UIAElementArray.prototype, {
     var ret = [];
     for (var i = 0; i < this.length; ++i) {
       var elem = this[i];
-      if (elem.isNotNil() && elem.name().match(pattern) !== null) {
+      if (elem.isNotNil && elem.isNotNil() && elem.name().match(pattern) !== null) {
         ret.push(elem);
       }
     }
@@ -45,7 +45,7 @@ extend(UIAElementArray.prototype, {
   firstWithNameRegex: function(pattern) {
     for (var i = 0; i < this.length; ++i) {
       var elem = this[i];
-      if (elem.isNotNil() && elem.name().match(pattern) !== null) return elem;
+      if (elem.isNotNil && elem.isNotNil() && elem.name().match(pattern) !== null) return elem;
     }
     return new UIAElementNil();
   }
